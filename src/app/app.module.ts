64e71a6755cf5153de913ab447386e36277ecdae
Bruno <bccaxias03.bc@outlook.com>
2022-05-19 21:21:25 -0300

    Criando Cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,24 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PokemonComponent
   ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatToolbarModule,
-        NgbModule
-    ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    NgbModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
